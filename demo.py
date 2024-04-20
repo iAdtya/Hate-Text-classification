07d44f9eba59c6e5300c3614a09067b7804c0685
iAdtya <adityakhedekar98906@gmail.com>
2024-04-21 00:58:34 +0530

    Refactor code for improved performance Update project dependencies Add new data ingestion artifacts Update model architecture Update app.py for prediction route Update constants for model evaluation

--- demo.py
+++ demo.py
@@ -1,15 +1,56 @@
-from text_classification.logger import logging
-from text_classification.exception import CustomException
-import sys
-from text_classification.configuration.gcloud_syncer import GCloudSync
+# from text_classification.logger import logging
+# from text_classification.exception import CustomException
+# import sys
+# from text_classification.configuration.gcloud_syncer import GCloudSync
 
-# logging.info("Welcome to Logging!! ")
+# # logging.info("Welcome to Logging!! ")
 
-# try:
-#     a = 2 / "0"
+# # try:
+# #     a = 2 / "0"
 
-# except Exception as e:
-#     raise CustomException(e, sys) from e
+# # except Exception as e:
+# #     raise CustomException(e, sys) from e
 
-obj = GCloudSync()
-obj.sync_folder_from_gcloud("hate-classification", "dataset.zip", "Download/dataset.zip")
+# obj = GCloudSync()
+# obj.sync_folder_from_gcloud("hate-classification", "dataset.zip", "Download/dataset.zip")
+
+
+# from fastbook import *
+# urls = search_images_ddg('grizzly bear', max_images=100)
+# len(urls),urls[0]
+
+# dest = Path('grizzly.jpg')
+# if not dest.exists(): download_url(urls[0], dest, show_progress=False)
+
+# im = Image.open(dest)
+# im.to_thumb(256,256)
+
+# searches ='forest', 'grizzly'
+# path = Path('bird_or_not')
+
+# if not path.exists():
+#   for o in searches:
+#     dest = (path/o)
+#     dest.mkdir(exist_ok=True)
+#     results = search_images_ddg(f'{o} photo')
+#     download_images (dest, urls=results[:200])
+#     resize_images (dest, max_size=400, dest=dest)
+
+# failed = verify_images(get_image_files(path))
+# failed.map(Path.unlink)
+
+# dls = DataBlock(
+#     blocks=(ImageBlock, CategoryBlock),
+#     get_items=get_image_files,
+#     splitter=RandomSplitter(valid_pct=0.2, seed=42),
+#     get_y=parent_label,
+#     item_tfms=[Resize(192,method='squish')]
+# ).dataloaders(path)
+
+# dls.show_batch(max_n=6)
+
+# learn = cnn_learner(dls,resnet18,metrics=error_rate)
+# learn.fine_tune(3)
+
+import keras
+print(keras.__version__)
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="hate-speech-classification",
+    name="hate-text",
     version="0.0.1",
     author="iAdtya",
     author_email="adityakhedekar98906@gmail.com",
